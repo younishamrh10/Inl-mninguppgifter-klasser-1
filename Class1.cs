df4afbfc1b0629b4eaf273b768b943cb56949b99
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-04-05 08:46:41 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämninguppgifter_klasser_1
+{
+    internal class Class1
+    {
+        private string märke;
+        private string modell;
+        private int årsmodell;
+        private int hästkrafter;
+        private int maxhastighet;
+
+        public Class1(string mä, string mo, int år, int hä, int ma)
+        {
+            märke = mä;
+            modell = mo;
+            årsmodell = år;
+            hästkrafter = hä;
+            maxhastighet = ma;
+        }
+
+        public Class1() { }
+
+        public string Märke
+        {
+            get { return märke; }
+            set { märke = value; }
+        }
+
+        public string Modell
+        {
+            get { return modell; }
+            set { modell = value; }
+        }
+
+        public int Årsmodell
+        {
+            get { return årsmodell; }
+            set { årsmodell = value; }
+        }
+
+        public int Hästkrafter
+        {
+            get { return hästkrafter; }
+            set { hästkrafter = value; }
+        }
+
+        public int Maxhastighet
+        {
+            get { return maxhastighet; }
+            set { maxhastighet = value; } 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Inlämninguppgifter_klasser_1
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            Class1 bil1 = new Class1("BMW", "m5", 2024, 500, 360);
+            Console.WriteLine("Bil 1:");
+            Console.WriteLine("Märke: " + bil1.Märke);
+            Console.WriteLine("Modell: " + bil1.Modell);
+            Console.WriteLine("Årsmodell: " + bil1.Årsmodell);
+            Console.WriteLine("Hästrkrafter: " + bil1.Hästkrafter);
+            Console.WriteLine(" MAxhastighet: " + bil1.Maxhastighet); 
+        }
+    }
+}
